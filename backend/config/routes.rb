14b29176466d05a5934721c54a9aa9305e876ded
Rene Wang <rene.wang1@hotmail.com>
2021-02-13 23:53:49 +0000

    create basic endpoints

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
-    resources :users
+    resources :users do
+      resources :favourites
+      resources :conversations
+    end
+    resources :login, only: [:create]
+    resources :register
+
   end
 end
