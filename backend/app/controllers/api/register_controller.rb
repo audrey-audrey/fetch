7399a6db205a1f114bde5929c9b62c0afd65f021
Rene Wang <rene.wang1@hotmail.com>
2021-02-14 01:44:17 +0000

    add logic for create using params

--- backend/app/controllers/api/register_controller.rb
+++ backend/app/controllers/api/register_controller.rb
@@ -1,2 +1,11 @@
 class Api::RegisterController < ApplicationController
+
+  def create
+    user = User.new(
+      name: params["name"], 
+      email: params["email"], 
+      password: params["password"]
+    )
+    render json: user
+  end
 end
