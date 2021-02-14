4403759b38c996f95fca460f10d470f717b7603c
Rene Wang <rene.wang1@hotmail.com>
2021-02-14 01:47:13 +0000

    add logic for show, create favourites

--- backend/app/controllers/api/favourites_controller.rb
+++ backend/app/controllers/api/favourites_controller.rb
@@ -1,2 +1,12 @@
 class Api::FavouritesController < ApplicationController
+
+  def show
+    favourites = Favourite.where(favouriter: params["user_id"])
+    render json: favourites
+  end
+
+  def create
+    favourite = Favourite.new(favouriter: params["user_id"], favouritee: params["favourite_id"])
+    render json: favourite
+  end
 end
